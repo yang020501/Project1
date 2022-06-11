23156587f995acd8e222976aaae2dc96d75c5c71
yang020501 <thaiduong020501@gmail.com>
2022-06-11 12:02:47 +0700

    aha

---
+++ frontend/src/components/OrderItem.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types';
+import numberWithCommas from '../utils/numberWithCommas';
+
+
+
+const OrderItem = props => {
+    const [item, setItem] = useState(props.item)
+    const [quantity, setquantity] = useState(props.item.quantity)
+    useEffect(() => {
+        setquantity(props.item.quantity)
+        setItem(props.item)
+    }, [props.item])
+    return (
+        <div className='cart-item'>
+            <div className="cart-item-image">
+                <img src={require(`../assets/${item.product.image1}`)} alt='' />
+            </div>
+            <div className="cart-item-info">
+                <div className="cart-item-info-name">
+                    {`${item.product.title} - ${item.color} - ${item.size}`}
+                </div>
+                <div className="cart-item-info-price">
+                    {numberWithCommas(Number(item.product.price))} đ
+                </div>
+                <div className="cart-item-info-quantity">
+                    <div className="product-info-item-quantity">
+
+                        <div className="product-info-item-quantity-input fs-5">
+                            Số lượng: {quantity}
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+OrderItem.propTypes = {
+    item: PropTypes.object
+}
+
+export default OrderItem
