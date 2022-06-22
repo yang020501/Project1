0b2847621c498b7e551e0f54591ee9db49aec329
Duy <81013045+legendzero456789@users.noreply.github.com>
2022-06-22 11:31:44 +0700

    ok

--- Backend/src/main/java/com/example/Backend/repository/ProductRepo.java
+++ Backend/src/main/java/com/example/Backend/repository/ProductRepo.java
@@ -10,23 +10,23 @@ import java.util.List;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Integer> {
-    @Query("SELECT new com.example.Backend.dto.ProductDto(p.id, p.title, p.id_cate, p.categorySlug ,p.image1, p.image2, p.price, p.slug, p.colors, " +
+    @Query("SELECT new com.example.Backend.dto.ProductDto(p.id, p.title, p.id_cate, p.categorySlug ,p.gender ,p.image1, p.image2, p.price, p.slug, p.colors, " +
             "p.size, p.descriptions) FROM Product p")
     List<ProductDto> getAll();
 
-    @Query("SELECT new com.example.Backend.dto.ProductDto(p.id, p.title, p.id_cate, p.categorySlug ,p.image1, p.image2, p.price, p.slug, p.colors, p.size," +
+    @Query("SELECT new com.example.Backend.dto.ProductDto(p.id, p.title, p.id_cate, p.categorySlug ,p.gender ,p.image1, p.image2, p.price, p.slug, p.colors, p.size," +
             "p.descriptions) FROM Product p WHERE p.id = ?1")
     ProductDto getDetail_byID(String id);
 
-    @Query("SELECT new com.example.Backend.dto.ProductDto(p.id, p.title, p.id_cate, p.categorySlug ,p.image1, p.image2, p.price, p.slug, p.colors, p.size," +
+    @Query("SELECT new com.example.Backend.dto.ProductDto(p.id, p.title, p.id_cate, p.categorySlug ,p.gender ,p.image1, p.image2, p.price, p.slug, p.colors, p.size," +
             "p.descriptions) FROM Product p WHERE p.id_cate = ?1")
     List<ProductDto> getAll_byCateID(String id_cate);
 
-    @Query("SELECT new com.example.Backend.dto.ProductDto(p.id, p.title, p.id_cate, p.categorySlug ,p.image1, p.image2, p.price, p.slug, p.colors, p.size," +
+    @Query("SELECT new com.example.Backend.dto.ProductDto(p.id, p.title, p.id_cate, p.categorySlug ,p.gender ,p.image1, p.image2, p.price, p.slug, p.colors, p.size," +
             "p.descriptions) FROM Product p WHERE p.categorySlug = ?1")
     List<ProductDto> getProduct_byCateSlug(String categorySlug);
 
-    @Query("SELECT new com.example.Backend.dto.ProductDto(p.id, p.title, p.id_cate, p.categorySlug ,p.image1, p.image2, p.price, p.slug, p.colors, p.size," +
+    @Query("SELECT new com.example.Backend.dto.ProductDto(p.id, p.title, p.id_cate, p.categorySlug ,p.gender ,p.image1, p.image2, p.price, p.slug, p.colors, p.size," +
             "p.descriptions) FROM Product p WHERE p.slug = ?1")
     List<ProductDto> getProduct_bySlug(String slug);
 
